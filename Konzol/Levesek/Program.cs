e0b006df0fff02009f51cd9a6e3550a440655443
Pistipisti94 <pistipisti57@gmail.com>
2024-04-17 12:59:40 +0200

    Leves mennyiség kiiratása konzolra

--- Konzol/Levesek/Program.cs
+++ Konzol/Levesek/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Levesek;
@@ -9,8 +10,27 @@ namespace Levesek
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            await LevesAdatok();
+            Console.Title = "Ugyfelek";
+
+
+        }
+        private static async Task LevesAdatok()
+        {            
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage responseMessage = await httpClient.GetAsync("http://localhost/Levesek-feladat/backend/index.php?levesek");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                string jsonString = await responseMessage.Content.ReadAsStringAsync();
+                var etelek = Etelek.FromJson(jsonString);
+               
+                    Console.WriteLine($"{etelek.Length}");
+                
+                Console.ReadLine();
+            }
         }
     }
+    
 }
