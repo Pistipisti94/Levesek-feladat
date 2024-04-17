25e3e3169ecf0cdc0ee1a242f0a928156a65555c
Pistipisti94 <pistipisti57@gmail.com>
2024-04-17 14:01:08 +0200

    post

--- backend/levesek/index.php
+++ backend/levesek/index.php
@@ -3,6 +3,9 @@ switch ($_SERVER['REQUEST_METHOD']) {
     case 'GET':
         require_once 'getLevesek.php';
         break;
+    case 'POST':
+        require_once 'postLevesek.php';
+        break;
     default:
         # code...
         break;
